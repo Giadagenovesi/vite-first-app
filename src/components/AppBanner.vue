c103d744daf941b40985309e6025d97d1f08d450
Giadagenovesi <giada.g@outlook.it>
2023-04-12 12:58:40 +0200

    First exaple with vite

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppBanner",
+    data() {
+        return {
+            text: "lorem ipsum eccccc"
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h2>Banner</h2>
+    <p> {{ text }} </p>
+</template>
+
+<style></style>
+  
